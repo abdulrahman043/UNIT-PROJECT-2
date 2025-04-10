78f2238a60c87aafcfe598944e4fbc9911656164
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-04-10 11:29:17 +0300

    live score with grid added

--- sportpost/matches/urls.py
+++ sportpost/matches/urls.py
@@ -4,6 +4,8 @@ app_name="matches"
 urlpatterns = [
     path("score/",views.live_score_view,name="live_score_view"),
         path("livescore/",views.live_score_basketball_view,name="live_score_basketball_view"),
+            path("live_score/",views.score,name="score"),
+
 
 
     
--- sportpost/matches/views.py
+++ sportpost/matches/views.py
@@ -6,7 +6,6 @@ from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
 def live_score_view(request:HttpRequest):
-    
     selected_date=request.GET.get("match_date")
     if selected_date:
         try:
@@ -44,4 +43,18 @@ def live_score_basketball_view(request:HttpRequest):
         days_list.append(day_dict)
     matches = Match.objects.filter(date=selected_date).order_by("time")
     
-    return render(request, "matches/live_score_basketball.html", {"matches": matches,"selected_date":selected_date,"days_list":days_list})
+    return render(request, "matches/live_score_basketball.html", {"matches": matches,"selected_date":selected_date,"days_list":days_list,"live_score":True})
+def score(request:HttpRequest):
+    selected_date=request.GET.get("match_date")
+    if selected_date:
+        try:
+            selected_date = datetime.datetime.strptime(selected_date, "%Y-%m-%d").date()
+        except ValueError:
+            selected_date = timezone.now().date()
+    else:
+        selected_date = timezone.now().date()
+    if not selected_date:
+        selected_date = timezone.now().date()
+    matches = Match.objects.filter(date=selected_date).order_by("time")
+    
+    return render(request, "matches/score.html", {"matches": matches,"selected_date":selected_date})
