c88795cae8d18c7d20f00eabc90dadc3e1523ca9
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-04-14 19:20:21 +0300

    ready to deploy

--- sportpost/sportpost/settings.py
+++ sportpost/sportpost/settings.py
@@ -18,7 +18,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://unit-project-2-production-38c7.up.railway.app",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
