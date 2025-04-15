77ea2261d744c555ef246909af97a8ff282b264a
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-04-15 09:56:58 +0300

    ready to deploy

--- sportpost/matches/management/commands/runapscheduler.py
+++ sportpost/matches/management/commands/runapscheduler.py
@@ -131,7 +131,7 @@ class Command(BaseCommand):
 
     scheduler.add_job(
       my_job,
-      trigger=CronTrigger(min="*/59"),  # Every 10 seconds
+      trigger=CronTrigger(minute="*/59"),  # Every 10 seconds
       id="my_job",  # The `id` assigned to each job MUST be unique
       max_instances=1,
       replace_existing=True,
